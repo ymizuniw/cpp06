d4edcdd5c678f1c3ce5fc1af2c56454dadc25c99
ymizuniw <newerdaysreki@gmail.com>
2026-04-27 17:54:31 +0900

    update: type idetification is implemented. Branch for each numeric type is next task

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -3,8 +3,22 @@
 #include <string>
 #include <iostream>
 #include <sstream>
+#include <limits>
+#include <exception>
 
-int validate_num(std::string num)
+typedef enum e_scalar_convert_status{
+    SC_STATUS_DOUBLE,
+    SC_STATUS_FLOAT,
+    SC_STATUS_INT,
+    SC_STATUS_CHAR,
+    SC_STATUS_MINUS_INF,
+    SC_STATUS_PLUS_INF,
+    SC_STATUS_NAN,
+    SC_STATUS_ERROR
+} t_scalar_convert_status;
+
+//this shoul return the specific types
+t_scalar_convert_status validate_num(std::string num)
 {
     // int part
     size_t i = 0;
@@ -15,11 +29,23 @@ int validate_num(std::string num)
         i++;
     }
     if (i==num.length())
-        return 1;
+        return (SC_STATUS_INT);
     if (num.at(i)=='.')
+    {
         i++;
-    if (i==num.length())
-        return (1);
+        if (i==num.length())
+            return (SC_STATUS_DOUBLE);
+        else if (num.at(i)!='f' && !std::isdigit(num.at(i)))
+            return (SC_STATUS_ERROR);
+    }
+    if (num.at(i)=='f')
+    {
+        i++;
+        if (i==num.length())
+            return (SC_STATUS_FLOAT);
+        else
+            return (SC_STATUS_ERROR);
+    }
     // fractional part
     while(i<num.length())
     {
@@ -28,26 +54,36 @@ int validate_num(std::string num)
         i++;
     }
     if (i==num.length())
-        return (1);
+        return (SC_STATUS_DOUBLE);
     if (num.at(i)=='f')
+    {
         i++;
-    else
-        return (0);
-    if (i==num.length())
+        if (i==num.length())
+            return (SC_STATUS_FLOAT);
+        else
+            return (SC_STATUS_ERROR);
+    }
+    return (SC_STATUS_ERROR);
+}
+
+int validate_char(std::string num)
+{
+    if(num.length()==1 && !std::isdigit(num.at(0)))
         return (1);
     return (0);
 }
 
-typedef enum e_scalar_convert_status{
-    SC_STATUS_CONVERTIBLE,
-    SC_STATUS_MINUS_INF,
-    SC_STATUS_PLUS_INF,
-    SC_STATUS_NAN,
-    SC_STATUS_ERROR
-} t_scalar_convert_status;
+
 
 void printResult(int num_type, std::string num)
 {
+    std::stringstream ss_int(num);
+    std::stringstream ss_float(num);
+    std::stringstream ss_double(num);
+    int converted_int = 0;
+    float converted_float = 0.f;
+    double converted_double = 0.0;
+
     if (num_type==SC_STATUS_MINUS_INF)
     {
         std::cout << "char: " << "impossible\n";
@@ -69,18 +105,23 @@ void printResult(int num_type, std::string num)
         std::cout << "float: " << "nanf\n";
         std::cout << "double: " << "nan" << std::endl;
     }
-    else if (num_type==SC_STATUS_CONVERTIBLE)
+    else if (num_type==SC_STATUS_DOUBLE)
     {
-        std::stringstream ss_int(num);
-        std::stringstream ss_float(num);
-        std::stringstream ss_double(num);
-        int converted_int = 0;
-        float converted_float = 0.f;
-        double converted_double = 0.0;
-        ss_int >> converted_int;
-        ss_float >> converted_float;
-        ss_double >> converted_double;
         
+    }
+    else if (num_type==SC_STATUS_FLOAT)
+    {
+        
+    }
+    else if (num_type==SC_STATUS_INT)
+    {
+        
+    }
+    else if (num_type==SC_STATUS_CHAR)
+    {
+        converted_int = static_cast<int>(num.at(0));
+        converted_float = static_cast<float>(num.at(0));
+        converted_double = static_cast<double>(num.at(0));
         if (std::isprint(converted_int))
             std::cout << "char: " << static_cast<unsigned char>(converted_int) << "\n";
         else
@@ -88,6 +129,10 @@ void printResult(int num_type, std::string num)
         std::cout << "int: " << converted_int << "\n";
         std::cout << "float: " << converted_float << "f\n";
         std::cout << "double: " << converted_double << std::endl;
+    }
+    else
+    {
+        std::cout << "[Invalid Argment]: " << num << std::endl;
     }
 }
 
@@ -105,22 +150,37 @@ void ScalarConverter::convert(std::string num){
     // 3. the num is more than one characters or non-displayable, char handles it properly.
     // 4. FLOAT_MIN > num || FLOAT_MAX < num and within double, how does it behave?
 
-    int num_type = SC_STATUS_ERROR;
+//     int num_type = SC_STATUS_ERROR;
+//     double dmax = std::numeric_limits<double>::max();
+//     double dmin = std::numeric_limits<double>::min();
+//     float fmax = std::numeric_limits<float>::max();
+//     float fmin = std::numeric_limits<float>::min();
 
+//     std::stringstream ss_dmax;
+//     std::stringstream ss_dmin;
+//     std::stringstream ss_fmax;
+//     std::stringstream ss_fmin;
+    
+//     ss_dmax<<dmax;
+//     ss_dmin<<dmin;
+//     ss_fmax<<fmax;
+//     ss_fmin<<fmin;
+// // std::cout << num << std::endl;
+//     std::string s_dmax = ss_dmax.str();
+//     std::string s_dmin = ss_dmin.str();
+//     std::string s_fmax = ss_fmax.str();
+//     std::string s_fmin = ss_fmin.str();
+
+    std::cout << fmax << std::endl;
     if (num=="-inff" || num=="-inf")
         num_type = SC_STATUS_MINUS_INF;
     else if (num=="inff" || num=="inf")
         num_type = SC_STATUS_PLUS_INF;
     else if (num=="nanf" || num=="nan")
         num_type = SC_STATUS_NAN;
-    else if (validate_num(num))
-        num_type = SC_STATUS_CONVERTIBLE;
+    else if (validate_char(num))
+        num_type = SC_STATUS_CHAR;
     else
-        num_type = SC_STATUS_ERROR;
+        num_type = validate_num(num);
     printResult(num_type, num);
-}
-
-int main(void)
-{
-    ScalarConverter::convert("1.4f");
 }
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -2,66 +2,20 @@
 #include <sstream>
 #include <iostream>
 #include <cctype>
+#include "ScalarConverter.hpp"
+#include <limits>
 
-int validate_num(std::string num)
+int main(int argc, char **argv)
 {
-    // int part
-    size_t i = 0;
-    while(i<num.length())
-    {
-        if (!std::isdigit(num.at(i)))
-            break;
-        i++;
-    }
-    if (i==num.length())
-        return 1;
-    if (num.at(i)=='.')
-        i++;
-    if (i==num.length())
+    if (argc!=2)
         return (1);
-    // fractional part
-    while(i<num.length())
-    {
-        if (!std::isdigit(num.at(i)))
-            break;
-        i++;
-    }
-    if (i==num.length())
-        return (1);
-    if (num.at(i)=='f')
-        i++;
-    else
-        return (0);
-    if (i==num.length())
-        return (1);
-    return (0);
-}
-
-int main(void)
-{
-    std::string num = "1f";
-    int valid_num = validate_num(num);
-    std::cout << valid_num << std::endl;
-    // std::stringstream ss(num);
-    // int inum;
-    // ss >> inum;
-    // ss.clear();
-    // std::cout << inum << std::endl;
-    // float f;
-    // ss >> f;
-    // ss.clear();
-    // std::cout << f << std::endl;
-    // double d;
-    // ss >> d;
-    // ss.clear();
-    // std::cout << f << std::endl;
-    char c = 41;
-    std::cout << c << std::endl;
-    std::cout << std::isprint(-1) << std::endl;
-
-    std::stringstream ss ("-3000000000000000000");
-    int res1 = 0;
-    ss>>res1;
-    std::cout << res1 << std::endl;
+    double dmax = std::numeric_limits<double>::max();
+    float fmax = std::numeric_limits<float>::max();
+    dmax = fmax;
+    std::stringstream ss;
+    ss << dmax;
+    std::string str = ss.str();
+    // (void)argv;
+    ScalarConverter::convert(argv[1]);
     return (0);
 }
