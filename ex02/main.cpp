39bdfeaf380c4df37c56530c31c2ab719166e655
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2026-04-30 18:08:23 +0900

    update: ex02 is written

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -2,57 +2,71 @@
 #include <cstring>
 #include <iostream>
 
-#include <ctime>
+#include "ABC.hpp"
+#include "Base.hpp"
 #include <cstdlib>
-
-class Base{
-    public:
-        virtual ~Base(void);
-};
-
-class A: public Base{
-
-};
-
-
-class B: public Base{
-
-};
-
-
-class C: public Base{
-
-};
-
-Base::~Base(void){}
+#include <ctime>
+#include <typeinfo>
 
 Base *generate(void){
-    int random = std::rand();
-    std::cout << RAND_MAX << ": " << random << std::endl;
-    if (0<=random && random<RAND_MAX/3)
-        return (new (A));
-    else if (RAND_MAX/3<=random && random<2*(RAND_MAX/3))
-        return (new (B));
-    else
-        return (new (C));
-    return (NULL);
+  int random = std::rand();
+  if (0 <= random && random < RAND_MAX / 3)
+    return (new (A));
+  else if (RAND_MAX / 3 <= random && random < 2 * (RAND_MAX / 3))
+    return (new (B));
+  else
+    return (new (C));
+  return (NULL);
 }
 
-int main(void)
-{
-    std::srand(static_cast<unsigned int>(std::time(NULL)));
-    Base *base = generate();
-    A *a = dynamic_cast<A *>(base);
-    B *b = dynamic_cast<B *>(base);
-    C *c = dynamic_cast<C*>(base);
-    if (a!=NULL)
-        std::cout << "Type: A" << std::endl;
-    if (b!=NULL)
-        std::cout << "Type: B" << std::endl;
-    if (c!=NULL)
-        std::cout << "Type: C" << std::endl;
-
-    return (0);
+void identify(Base *p) {
+  A *a = dynamic_cast<A *>(p);
+  B *b = dynamic_cast<B *>(p);
+  C *c = dynamic_cast<C *>(p);
+  if (a != NULL)
+    std::cout << "PtrType: A" << std::endl;
+  if (b != NULL)
+    std::cout << "PtrType: B" << std::endl;
+  if (c != NULL)
+    std::cout << "PtrType: C" << std::endl;
 }
 
-// https://cpprefjp.github.io/reference/typeinfo/bad_cast.html
+void identify(Base &p) {
+  int casted = 1;
+  try {
+    A a = dynamic_cast<A &>(p);
+  } catch (std::bad_cast &e) {
+    casted = 0;
+  }
+  if (casted)
+    std::cout << "RefType: A" << std::endl;
+  casted = 1;
+  try {
+    B b = dynamic_cast<B &>(p);
+  } catch (std::bad_cast &e) {
+    casted = 0;
+  }
+  if (casted)
+    std::cout << "RefType: B" << std::endl;
+  casted = 1;
+  try {
+    C c = dynamic_cast<C &>(p);
+  } catch (std::bad_cast &e) {
+    casted = 0;
+  }
+  if (casted)
+    std::cout << "RefType: C" << std::endl;
+}
+
+int main(void) {
+  std::srand(static_cast<unsigned int>(std::time(NULL)));
+  Base *base = generate();
+  try {
+    identify(base);
+    identify(*base);
+  } catch (...) {
+    delete (base);
+  }
+  delete (base);
+  return (0);
+}
