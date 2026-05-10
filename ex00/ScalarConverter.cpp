8b193700ce5bb85b7ede965e306542d2110636f0
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2026-05-10 14:56:32 +0900

    fix: struct and enum was defined in .cpp, and moved it to .hpp

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -5,34 +5,6 @@
 #include <sstream>
 #include <string>
 
-typedef enum e_scalar_convert_status {
-  SC_STATUS_CHAR,
-  SC_STATUS_INT,
-  SC_STATUS_FLOAT,
-  SC_STATUS_DOUBLE,
-  SC_STATUS_MINUS_INFF,
-  SC_STATUS_PLUS_INFF,
-  SC_STATUS_MINUS_INF,
-  SC_STATUS_PLUS_INF,
-  SC_STATUS_NANF,
-  SC_STATUS_NAN,
-  SC_STATUS_ERROR
-} t_scalar_convert_status;
-
-typedef struct s_scalar_values {
-  char sc_char;
-  int sc_int;
-  float sc_float;
-  double sc_double;
-  int sc_nanf;
-  int sc_nan;
-  int sc_char_non_print;
-  int sc_char_impossible;
-  int sc_int_impossible;
-  int sc_float_impossible;
-  int sc_double_impossible;
-} t_scalar_values;
-
 // this shoul return the specific types
 t_scalar_convert_status validate_num(std::string num) {
   // int part
--- ex00/ScalarConverter.hpp
+++ ex00/ScalarConverter.hpp
@@ -8,4 +8,32 @@ public:
   static void convert(std::string num);
 };
 
+typedef enum e_scalar_convert_status {
+  SC_STATUS_CHAR,
+  SC_STATUS_INT,
+  SC_STATUS_FLOAT,
+  SC_STATUS_DOUBLE,
+  SC_STATUS_MINUS_INFF,
+  SC_STATUS_PLUS_INFF,
+  SC_STATUS_MINUS_INF,
+  SC_STATUS_PLUS_INF,
+  SC_STATUS_NANF,
+  SC_STATUS_NAN,
+  SC_STATUS_ERROR
+} t_scalar_convert_status;
+
+typedef struct s_scalar_values {
+  char sc_char;
+  int sc_int;
+  float sc_float;
+  double sc_double;
+  int sc_nanf;
+  int sc_nan;
+  int sc_char_non_print;
+  int sc_char_impossible;
+  int sc_int_impossible;
+  int sc_float_impossible;
+  int sc_double_impossible;
+} t_scalar_values;
+
 #endif
