ecd6fa000f298f3f05bee6322c8f3df5db935888
Yugo Mizuniwa <ymizuniw@c6r2s14.42tokyo.jp>
2026-05-10 14:13:50 +0900

    fix: Makefile PHONEY->PHONY, ex02 print data contents to see serialize/deserialize results

--- ex01/Data.hpp
+++ ex01/Data.hpp
@@ -1,8 +1,11 @@
 #ifndef DATA_HPP
 #define DATA_HPP
 
+#include <string>
+
 struct Data {
-  int val;
+  int val_int;
+  std::string val_str;
 };
 
 #endif
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -5,13 +5,17 @@
 
 int main(void) {
   Data data;
-  data.val = 0;
+  data.val_int = 0;
+  data.val_str = "str";
   uintptr_t raw = Serializer::serialize(&data);
-  std::cout << raw << std::endl;
   Data *ptr = Serializer::deserialize(raw);
   if (&data == ptr)
-    std::cout << "Match: " << &data << " and " << ptr << std::endl;
+    std::cout << "Address Match: " << &data << " and " << ptr << std::endl;
   else
-    std::cout << "Not Match: " << &data << " and " << ptr << std::endl;
+    std::cout << "Address Not Match: " << &data << " and " << ptr << std::endl;
+  std::cout << "data.val_int: " << data.val_int << std::endl;
+  std::cout << "data.val_str: " << data.val_str << std::endl;
+  std::cout << "ptr->val_int: " << ptr->val_int << std::endl;
+  std::cout << "ptr->val_str: " << ptr->val_str << std::endl;
   return (0);
 }
